eb91535c01047bf496bed16226f336773c37b308
Knut Dundas Morå <fysikk.dundasmora.no>
2025-05-12 03:25:24 -0700

    initial commit

---
+++ fogtask/fogtask.py
@@ -0,0 +1,7 @@
+import numpy as np
+import scipy.stats as sps
+import yaml
+from importlib_resources import files
+
+def test_util():
+    print("I'm helping!"#)
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(
+        name="fogtask",
+        version = "0.3"
+        package_dir = {"fogtask":"fogtask"},
+        packages=find_packages(),
+        author="Maike Doerenkamp,Robert James,Knut Morå",
+        author_email="fysikk@dundasmora.no",
+        description="Code for XLZD requirements task force",
+        )
